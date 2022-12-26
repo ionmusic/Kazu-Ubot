16ee6a4f15a8d5788a371398e5abd6ddc6279899
IONBOT <113283570+ionmusic@users.noreply.github.com>
2022-12-26 17:51:28 +0700

    Update connections.py

--- Kazu/startup/connections.py
+++ Kazu/startup/connections.py
@@ -15,7 +15,7 @@ from telethon.sessions.string import _STRUCT_PREFORMAT, CURRENT_VERSION, StringS
 
 from ..configs import Var
 from . import *
-from .BaseClient import AyraClient
+from .BaseClient import KazuClient
 
 _PYRO_FORM = {351: ">B?256sI?", 356: ">B?256sQ?", 362: ">BI?256sQ?"}
 
@@ -72,13 +72,13 @@ def validate_session(session, logger=LOGS, _exit=True):
         sys.exit()
 
 
-def vc_connection(udB, ayra_bot):
+def vc_connection(udB, kazu_bot):
     from strings import get_string
     VC_SESSION = Var.VC_SESSION or udB.get_key("VC_SESSION")
     if VC_SESSION and VC_SESSION != Var.SESSION:
         LOGS.info("Memulai VcClient.")
         try:
-            return AyraClient(
+            return KazuClient(
                 validate_session(VC_SESSION, _exit=False),
                 log_attempt=False,
                 exit_on_error=False
@@ -89,4 +89,4 @@ def vc_connection(udB, ayra_bot):
         except Exception as er:
             LOGS.info("Saat membuat Klien untuk VC.")
             LOGS.exception(er)
-    return ayra_bot
+    return kazu_bot
