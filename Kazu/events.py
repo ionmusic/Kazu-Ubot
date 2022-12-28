c3322aac7aedff768d9cefaad1c1e7509c90b4df
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2022-12-29 05:16:56 +0700

    Update events.py

--- Kazu/events.py
+++ Kazu/events.py
@@ -17,7 +17,7 @@ from traceback import format_exc
 from telethon import events
 
 from .dB import CMD_HANDLER, CMD_LIST, DEFAULT, DEVLIST
-from . import asst
+from ._misc._supporter import bot
 
 
 def kazu_cmd(pattern=None, command=None, **args):
