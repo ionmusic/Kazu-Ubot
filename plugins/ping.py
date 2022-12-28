37826eb25f3c340e92a5748082c156f05e868f0c
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2022-12-29 00:37:53 +0700

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -52,7 +52,7 @@ async def get_readable_time(seconds: int) -> str:
     return up_time
 
 
-@kazu_cmd(incoming=True, from_users=DEVLIST, pattern=r"^Cping|ping$")
+@kazu_cmd(incoming=True, from_users=DEVLIST, pattern=r"^Cping$")
 async def _(ping):
     uptime = await get_readable_time((time.time() - StartTime))
     start = datetime.now()
