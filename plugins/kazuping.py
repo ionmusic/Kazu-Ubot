99310af027a1dae564c50f7c2ab6e5f0208f7f11
IONBOT <113283570+ionmusic@users.noreply.github.com>
2022-12-28 19:10:39 +0700

    Update kazuping.py

--- plugins/kazuping.py
+++ plugins/kazuping.py
@@ -17,8 +17,6 @@ from datetime import datetime
 from secrets import choice
 
 
-from Kazu.startup.utils import CMD_HNDLR
-from Kazu.startup.utils import HELP, StartTime
 from Kazu.dB import DEVLIST
 from Kazu.events import register
 from .ping import get_readable_time
