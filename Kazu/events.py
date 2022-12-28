e97700ce2c0c7e1a5ec99120dace693a01c96c4b
IONBOT <113283570+ionmusic@users.noreply.github.com>
2022-12-28 18:28:43 +0700

    Create events.py

---
+++ Kazu/events.py
@@ -0,0 +1,57 @@
+import inspect
+import re
+import sys
+
+from telethon import events
+
+from Kazu.dB import DEVLIST, DEFAULT
+
+
+
+
+def register(**args):
+    """Register a new event."""
+    pattern = args.get("pattern")
+    disable_edited = args.get("disable_edited", False)
+    ignore_unsafe = args.get("ignore_unsafe", False)
+    unsafe_pattern = r"^[^/!#@\$A-Za-z]"
+    groups_only = args.get("groups_only", False)
+    trigger_on_fwd = args.get("trigger_on_fwd", False)
+    disable_errors = args.get("disable_errors", False)
+    insecure = args.get("insecure", False)
+    args.get("sudo", False)
+    args.get("own", False)
+
+    if pattern is not None and not pattern.startswith("(?i)"):
+        args["pattern"] = "(?i)" + pattern
+
+    if "disable_edited" in args:
+        del args["disable_edited"]
+
+    if "sudo" in args:
+        del args["sudo"]
+        args["incoming"] = True
+        args["from_users"] = DEVLIST
+
+    if "ignore_unsafe" in args:
+        del args["ignore_unsafe"]
+
+    if "groups_only" in args:
+        del args["groups_only"]
+
+    if "disable_errors" in args:
+        del args["disable_errors"]
+
+    if "trigger_on_fwd" in args:
+        del args["trigger_on_fwd"]
+
+    if "own" in args:
+        del args["own"]
+        args["incoming"] = True
+        args["from_users"] = DEFAULT
+
+    if "insecure" in args:
+        del args["insecure"]
+
+    if pattern and not ignore_unsafe:
+        args["pattern"] = pattern.replace("^.", unsafe_pattern, 1)
