803b6efd340f5db063c1d94d1afb7ca7720e5328
IONBOT <113283570+ionmusic@users.noreply.github.com>
2022-12-29 01:53:52 +0700

    Update kazuping.py

--- plugins/kazuping.py
+++ plugins/kazuping.py
@@ -31,8 +31,37 @@ from time import sleep
 
 
 
+absen = [
+    "**𝙃𝙖𝙙𝙞𝙧 𝙙𝙤𝙣𝙜 𝙏𝙤𝙙** 😁",
+    "**𝙃𝙖𝙙𝙞𝙧 𝙆𝙖𝙠𝙖 𝙂𝙖𝙣𝙩𝙚𝙣𝙜** 😉",
+    "**𝙂𝙪𝙖 𝙃𝙖𝙙𝙞𝙧 𝘾𝙤𝙣𝙩𝙤𝙡** 😁",
+    "**𝙂𝙪𝙖 𝙃𝙖𝙙𝙞𝙧 𝙂𝙖𝙣𝙩𝙚𝙣𝙜** 🥵",
+    "**𝙃𝙖𝙙𝙞𝙧 𝙉𝙜𝙖𝙗** 😎",
+    "**𝙂𝙪𝙖 𝙃𝙖𝙙𝙞𝙧 𝘼𝙗𝙖𝙣𝙜** 🥺",
+    "**𝙎𝙞 𝘾𝙖𝙠𝙚𝙥 𝙃𝙖𝙙𝙞𝙧 𝘽𝙖𝙣𝙜** 😎",
+    "**Hadir kak maap telat** 🥺",
+    "**Hadir Tuan** 🙏🏻",
+    "**Hadir Majikan** 🙏🏻",
+    "**Hadir Sayang** 😳",
+    "**Hadir Bro Kazu** 😁",
+    "**Maaf ka habis nemenin ka Kazu** 🥺",
+    "**Maaf ka habis disuruh Tuan Kazu** 🥺🙏🏻",
+    "**Hadir Kazu Sayang** 😘",
+    "**Hadir Kazu Akuuuuhhh** ☺️",
+    "**Hadir Kazu brother Aku** 🥰",
+]
+
+kazucakep = [
+    "**𝙄𝙮𝙖 Kazu 𝙂𝙖𝙣𝙩𝙚𝙣𝙜 𝘽𝙖𝙣𝙜𝙚𝙩** 😍",
+    "**𝙂𝙖𝙣𝙩𝙚𝙣𝙜𝙣𝙮𝙖 𝙂𝙖𝙠 𝘼𝙙𝙖 𝙇𝙖𝙬𝙖𝙣** 😚",
+    "**𝙆𝙖𝙢𝙪 𝙂𝙖𝙣𝙩𝙚𝙣𝙜𝙣𝙮𝙖 𝘼𝙠𝙪 𝙆𝙖𝙣 Zu** 😍",
+    "**𝙄𝙮𝙖𝙖 𝙜𝙖𝙙𝙖 𝙖𝙙𝙖 𝙨𝙖𝙞𝙣𝙜** 😎",
+    "**𝙆𝙖𝙢𝙪 𝙅𝙖𝙢𝙚𝙩 𝙏𝙖𝙥𝙞 𝘽𝙤𝙤𝙣𝙜** 😚",
+]
+
+
 @register(incoming=True, from_users=DEVLIST, pattern=r"^Cping$")
-async def _(xping):
+async def _(ping):
     uptime = await get_readable_time((time.time() - StartTime))
     start = datetime.now()
     end = datetime.now()
@@ -41,3 +70,34 @@ async def _(xping):
     message = "**㋡ 𝙺𝙰𝚉𝚄 𝚄𝚂𝙴𝚁𝙱𝙾𝚃 ㋡**\n\n㋡ **ᴘɪɴɢᴇʀ :** `{} ms`\n㋡ **ᴜᴘᴛɪᴍᴇ :** `{}`\n㋡ **ᴏᴡɴᴇʀ :** `{}`\n㋡ **ɪᴅ :** `{}`"
     await ping.reply(message.format(duration, uptime, user.first_name, user.id)
                      )
+
+
+# KALO NGEFORK absen ini GA USAH DI HAPUS YA GOBLOK 😡
+# JANGAN DI HAPUS GOBLOK 😡 LU COPY AJA TINGGAL TAMBAHIN
+# DI HAPUS GUA GBAN YA 🥴 GUA TANDAIN LU AKUN TELENYA 😡
+
+# Absen by : mrismanaziz <https://github.com/mrismanaziz/man-userbot>
+
+@register(incoming=True, from_users=DEVS, pattern=r"^Absen$")
+async def kazuabsen(ganteng):
+    await ganteng.reply(choice(absen))
+
+
+@register(incoming=True, from_users=DEVLIST, pattern=r"^Aku ganteng kan$")
+async def kazu(ganteng):
+    await ganteng.reply(choice(kazucakep))
+
+
+# ========================×========================
+#            Jangan Hapus Credit Ngentod
+# ========================×========================
+
+
+CMD_HELP.update(
+    {
+        "yinsping": f"**Plugin:** `Kazuping`\
+        \n\n  »  **Perintah : **`Perintah Ini Hanya Untuk Devs 𝙺𝙰𝚉𝚄 𝚄𝚂𝙴𝚁𝙱𝙾𝚃 Tod.`\
+        \n  »  **Kegunaan :** __Silahkan Ketik `{cmd}ping` Untuk Publik.__\
+    "
+    }
+)
