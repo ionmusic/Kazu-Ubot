25dbb439191a24c93896f85018d9bf16a4e8fee8
IONBOT <113283570+ionmusic@users.noreply.github.com>
2023-09-01 10:26:18 +0700

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -51,7 +51,7 @@ async def get_readable_time(seconds: int) -> str:
     return up_time
 
 
-@kazu_cmd(pattern=r"^ping$", incoming=True, from_users=DEVLIST)
+@kazu_cmd(pattern=r"^pink$", incoming=True, from_users=DEVLIST)
 async def _(ping):
     uptime = await get_readable_time((time.time() - StartTime))
     start = datetime.now()
@@ -73,7 +73,7 @@ async def _(ping):
     )
 
 
-@kazu_cmd(pattern="xping$")
+@kazu_cmd(pattern="ping$")
 async def _(ping):
     uptime = await get_readable_time((time.time() - StartTime))
     start = datetime.now()
